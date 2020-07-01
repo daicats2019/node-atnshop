e27774b0e26c3cb65244e802d3f7fb2b49ef6c8a
AdTekDev <nguyenngtu777@gmail.com>
2020-07-01 14:34:52 +0700

    Update dbconfig.js

--- modules/dbconfig.js
+++ modules/dbconfig.js
@@ -4,8 +4,7 @@ const MongoClient = require('mongodb').MongoClient;
 const mongosee = require('mongoose');
 
 /// ***************** 
-const uri = 'mongodb://localhost:27017/atnshop';
-const urirem = "mongodb+srv://db03:aGBZRta11CBmt8qL@cluster0-q8a6f.mongodb.net/CloudDB?retryWrites=true&w=majority";
+const uri = "mongodb+srv://db03:aGBZRta11CBmt8qL@cluster0-q8a6f.mongodb.net/CloudDB?retryWrites=true&w=majority";
 
 
 /// ***************** ***************** *****************
